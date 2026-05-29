ff2f8e97811033a263bde97ed14f05034188fc19
Gabriel Fidelles <gabrielfidellesc@hotmail.com>
2026-05-29 16:16:47 -0300

    init: inicializando projeto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-gray-900">
+      <h1 className="text-4xl font-bold text-center py-16">De Leitor a Escritor</h1>
+    </div>
+  )
+}
+
+export default App
